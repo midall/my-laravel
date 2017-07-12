d9293bbd2bdf18b78c6f4e1e6539bb0c81e676f7
midall <mi_dall@hotmail.com>
2017-07-12 23:24:28 +0100

    Add documentation and log files

---
+++ changelog.md
@@ -0,0 +1,16 @@
+# Change Log
+
+-----------------------------------------------------------------------------------------
+Version 0.1 - beta - July 12th, 2017
+-----------------------------------------------------------------------------------------
+
+- initial commit, setup project environment
+- install laravel
+- add project folder
+- first steps in Laravel
+- add first pages and layout
+- firts page in Laravel
+- sidebar file
+- install Node.js
+- add documentation
+- add changelog.md
---
+++ documentation/index.php
@@ -0,0 +1,314 @@
+<?php
+require '../constants.php';
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+	<!--<![endif]-->
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="Michael Nt">
+		<meta name="keywords" content="">
+
+		<title>MyLaravel | Documentation by Michael Nt</title>
+
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/stroke.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+		<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+		<link rel="stylesheet" type="text/css" href="js/syntax-highlighter/styles/shCore.css" media="all">
+		<link rel="stylesheet" type="text/css" href="js/syntax-highlighter/styles/shThemeRDark.css" media="all">
+
+		<!-- CUSTOM -->
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		<div id="wrapper">
+
+			<div class="container">
+
+				<section id="top" class="section docs-heading">
+
+					<div class="row">
+						<div class="col-md-12">
+							<div class="big-title text-center">
+								<h1>My Laravel</h1>
+								<p class="lead">Documentation for the version 0.1beta</p>
+							</div>
+							<!-- end title -->
+						</div>
+						<!-- end 12 -->
+					</div>
+					<!-- end row -->
+
+					<hr>
+
+				</section>
+				<!-- end section -->
+
+				<div class="row">
+
+					<div class="col-md-3">
+						<nav class="docs-sidebar" data-spy="affix" data-offset-top="300" data-offset-bottom="200" role="navigation">
+							<ul class="nav">
+								<li><a href="#line1">About the project</a></li>
+								<li><a href="#line2">How to Install</a></li>
+								<li><a href="#line3"></a>
+									<ul class="nav">
+										<li><a href="#line3_1"></a></li>
+										<li><a href="#line3_2"></a></li>
+										<li><a href="#line3_3"></a></li>
+									</ul>
+								</li>
+								<li><a href="#line4">References</a></li>
+								<li><a href="#line5">Version History (Changelog)</a></li>
+								<li><a href="#line6">Contact Me</a></li>
+							</ul>
+						</nav >
+					</div>
+					<div class="col-md-9">
+						<section id="line1" class="welcome">
+
+							<div class="row">
+								<div class="col-md-12 left-align">
+									<h2 class="dark-text">About the project<hr></h2>
+									<div class="row">
+
+										<div class="col-md-12 full">
+											<div class="intro1">
+												<ul>
+													<li><strong>Item Name : </strong>My Laravel</li>
+													<li><strong>Item Version : </strong> v 0.1</li>
+													<li><strong>Author  : </strong> <a href="https://ntallas.eu/" target="_blank"><?php echo AUTHOR; ?></a></li>
+													<li><strong>GitHub repository: </strong> <a href="https://github.com/midall/my-laravel" target="_blank">My Laravel project</a></li>
+												</ul>
+											</div>
+
+											<hr>
+											<div>
+												<p>First of all, thank you so much for downloading this repository.
+													<strong>You are awesome!</strong>
+													<br> You are entitled to get all free lifetime upcoming versions to this product.
+												</p>
+												
+												<p>This documentation is to help you regarding each step of customization. 
+													Please go through the documentation carefully to understand how this
+													application is made and how to edit this properly.
+													Knowledge of PHP, MySQL, JS, HTML and CSS  is required to customize this application. </p>
+												
+												<h4>Test Environment</h4>
+												<p>The application has been tested in: (NOT prerequisites)</p>
+												<ol>
+													<li>Apache 2.4.23</li>
+													<li>PHP 7.0.10</li>
+													<li>MySQL 5.7.14</li>
+												</ol>
+												<div class="intro2 clearfix">
+													<p>
+														<i class="fa fa-exclamation-triangle"></i>
+														Be careful while editing the software. If not edited properly, the functionality may break completely.
+														<br> No support is provided for faulty customization.
+													</p>
+												</div>
+												
+												<h4>Enhancements</h4>
+												<p></p>
+											</div>
+										</div>
+
+									</div>
+									<!-- end row -->
+								</div>
+							</div>
+						</section>
+
+						<section id="line2" class="section">
+
+							<div class="row">
+								<div class="col-md-12 left-align">
+									<h2 class="dark-text">How to Install<a href="#top">#back to top</a><hr></h2>
+								</div>
+								<!-- end col -->
+							</div>
+							<!-- end row -->
+							
+							<div class="row">
+								<div class="col-md-12">
+									<ol>
+										<li>Download the package</li>
+										<li>Copy-paste them into your web directory (local or remote)</li>
+										<li></li>
+										<li></li>
+										<li></li>
+										<li></li>
+									</ol>
+								</div>
+
+							</div>
+
+						</section>
+						<!-- end section -->
+
+						<section id="line3" class="section">
+
+							<div class="row">
+								<div class="col-md-12 left-align">
+									<h2 class="dark-text">Empty <a href="#top">#back to top</a><hr></h2>
+								</div>
+								<!-- end col -->
+							</div>
+							<!-- end row -->
+
+							<div class="row">
+								<div class="col-md-12">
+									<h4 id="line3_1">Empty</h4>
+									<p></p>
+									<br />
+									
+									<h4 id="line3_2"></h4>
+									<p></p>
+									<br />
+									
+									<h4 id="line3_3"></h4>
+									<p><strong></strong></p>
+									
+								</div>
+							</div>
+
+						</section>
+						<!-- end section -->
+						
+						<section id="line4" class="section">
+
+							<div class="row">
+								<div class="col-md-12 left-align">
+									<h2 class="dark-text"> Empty<a href="#top">#back to top</a><hr></h2>
+								</div>
+								<!-- end col -->
+							</div>
+							<!-- end row -->
+
+							<div class="row">
+								<div class="col-md-12">
+									<h4>Useful Links and Reference</h4>
+									<p>Find below useful links and SCORM material that used for this development:</p>
+									<ol>
+										<li><a href="#" target="_blank">Link</a></li>
+										<li><a href="#" target="_blank">Link</a></li>
+										<li><a href="#" target="_blank">Link</a></li>
+									</ol>
+								</div>
+							</div>
+							<!-- end row -->
+
+						</section>
+						<!-- end section -->
+						
+						<section id="line5" class="section">
+
+							<div class="row">
+								<div class="col-md-12 left-align">
+									<h2 class="dark-text">Version History (Changelog) <a href="#top">#back to top</a><hr></h2>
+								</div>
+								<!-- end col -->
+							</div>
+							<!-- end row -->
+
+							<div class="row">
+								<div class="col-md-12">
+
+									<p>You can find the version history on file <strong>changelog.md</strong> .</p>
+
+									<hr>
+
+									<h4>Changelog</h4>
+
+									<pre class="brush: html">
+										<?php
+										$handle = fopen( '../changelog.md', 'r');
+										while( ( $line = fgets( $handle ) ) !== false )
+										{
+											echo $line;
+										}
+										fclose($handle);
+										?>
+									</pre>
+								</div>
+							</div>
+							<!-- end row -->
+
+						</section>
+						<!-- end section -->
+						
+						<section id="line6" class="section">
+
+							<div class="row">
+								<div class="col-md-12 left-align">
+									<h2 class="dark-text">Contact Me <a href="#top">#back to top</a><hr></h2>
+								</div>
+								<!-- end col -->
+							</div>
+							<!-- end row -->
+
+							<div class="row">
+								<div class="col-md-12">
+									<p> Please feel free to visit <a href="https://ntallas.eu/" target="_blank">ntallas.eu</a> and contact me</a>
+									
+								</div>
+							</div>
+							<!-- end row -->
+
+						</section>
+						<!-- end section -->
+					</div>
+					<!-- // end .col -->
+
+				</div>
+				<!-- // end .row -->
+
+			</div>
+			<!-- // end container -->
+
+		</div>
+		<!-- end wrapper -->
+
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/retina.js"></script>
+		<script src="js/jquery.fitvids.js"></script>
+		<script src="js/wow.js"></script>
+		<script src="js/jquery.prettyPhoto.js"></script>
+
+		<!-- CUSTOM PLUGINS -->
+		<script src="js/custom.js"></script>
+		<script src="js/main.js"></script>
+
+		<script src="js/syntax-highlighter/scripts/shCore.js"></script>
+		<script src="js/syntax-highlighter/scripts/shBrushXml.js"></script>
+		<script src="js/syntax-highlighter/scripts/shBrushCss.js"></script>
+		<script src="js/syntax-highlighter/scripts/shBrushJScript.js"></script>
+
+	</body>
+
+</html>
