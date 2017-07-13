afd1712ae901baddd4c7c4b27d823e432ed3be13
midall <mi_dall@hotmail.com>
2017-07-13 21:09:59 +0100

    Change IDE

--- documentation/index.php
+++ documentation/index.php
@@ -9,7 +9,6 @@ require '../constants.php';
 <html lang="en">
 	<!--<![endif]-->
 	<head>
-
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
